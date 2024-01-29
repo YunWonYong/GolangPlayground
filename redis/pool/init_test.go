d753e5035149de9264bde6b4bcb6112517f3c6e0
wonyong.yun <wonyong.yun@playlinks.com>
2024-01-29 16:50:45 +0900

    redis pool package

---
+++ redis/pool/init_test.go
@@ -0,0 +1,53 @@
+package pool
+
+import (
+	"context"
+	"testing"
+	"time"
+)
+
+func TestRedisPoolInit(t *testing.T) {
+	options := new(RedisPoolInitOptions)
+	options.Network = "tcp"
+	options.Address = "localhost:6379"
+	timeoutSec := time.Duration(5) * time.Second
+	options.ConnectTimeout = timeoutSec
+	options.WriteTimeout = timeoutSec
+	options.ReadTimeout = timeoutSec
+	options.MaxIdle = 10
+	options.Database = 0
+
+	p, err := NewRedisPool(context.TODO(), options)
+
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	if p == nil {
+		t.Fail()
+	}
+
+	if err := PingTest(p.Pool); err != nil {
+		t.Error(err)
+		return
+	}
+}
+
+func TestRedisPoolInitPingTest(t *testing.T) {
+	options := new(RedisPoolInitOptions)
+	options.Network = "tcp"
+	options.Address = "localhost:6379"
+	timeoutSec := time.Duration(5) * time.Second
+	options.ConnectTimeout = timeoutSec
+	options.WriteTimeout = timeoutSec
+	options.ReadTimeout = timeoutSec
+	options.MaxIdle = 10
+	options.Database = 0
+	options.PingTestFlag = true
+
+	if _, err := NewRedisPool(context.TODO(), options); err != nil {
+		t.Error(err)
+		return
+	}
+}
